6d53ab5deaf27680aac14f7ae68b2420a26b0669
bodhibrdr <julie.gumerman@gmail.com>
2020-03-11 13:06:00 -0600

    blockchain file set up in client_mining folder

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -34,8 +34,8 @@ class Blockchain(object):
 
             'index': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions': self.current_transactions
-            'proof': proof
+            'transactions': self.current_transactions,
+            'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
 
@@ -90,9 +90,14 @@ class Blockchain(object):
         in an effort to find a number that is a valid proof
         :return: A valid proof for the provided block
         """
-        # TODO
-        pass
-        # return proof
+
+        block_string = json.dumps(block, sort_keys=True)
+
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
+        return proof
+
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -106,9 +111,11 @@ class Blockchain(object):
         correct number of leading zeroes.
         :return: True if the resulting hash is a valid proof, False otherwise
         """
-        # TODO
-        pass
-        # return True or False
+        guess = f"{block_string}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:3] == "000"
+
 
 
 # Instantiate our Node
@@ -124,11 +131,14 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-
+    proof = blockchain.proof_of_work(blockchain.last_block)
     # Forge the new Block by adding it to the chain with the proof
+    previous_hash = blockchain.hash(blockchain.last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
 
     response = {
-        # TODO: Send a JSON response with the new block
+        'new_block': block
     }
 
     return jsonify(response), 200
@@ -137,7 +147,8 @@ def mine():
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        "message": "Wheee!!!"
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain)
     }
     return jsonify(response), 200
 
